d39b450a2e3d197cc5971c57934edbdda289ce0b
Thushan W A R T <rusiththarindu2@gmail.com>
2024-03-17 19:46:58 +0530

    Bottom Navigation Created
    Bottom Navigation is created and new pages (create, activity, profile, categories, HomeTwo)  have been uploaded

---
+++ PurrfectPal/src/screens/HomeTwo.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeTwo = () => {
+  return (
+    <View>
+      <Text>HomeTwo</Text>
+    </View>
+  )
+}
+
+export default HomeTwo
+
+const styles = StyleSheet.create({})
